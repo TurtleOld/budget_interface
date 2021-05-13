2df0396fefceb01421511553877e93eec26f94ee
TurtleOld <shurpo.alexander@gmail.com>
2021-05-13 07:01:04 +0300

    change size window, added column total sum

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from settings_database import cursor, connection
 
 window = tkinter.Tk()
 window.title("Домашняя бухгалтерия")
-window.geometry("800x400")
+window.geometry("910x400")
 window.iconbitmap(r"ico\victory_43837.ico")
 
 if connection:
@@ -36,7 +36,7 @@ def get_all_from_database():
 
 
 def get_all_info_receipt():
-    columns = ("#1", "#2", "#3", "#4", "#5", "#6")
+    columns = ("#1", "#2", "#3", "#4", "#5", "#6", "#7")
     tree = ttk.Treeview(show="headings", columns=columns)
     tree.heading("#1", text="Дата")
     tree.column("#1", width=75)
@@ -50,20 +50,22 @@ def get_all_info_receipt():
     tree.column("#5", width=75)
     tree.heading("#6", text="Сумма за товар")
     tree.column("#6", width=110)
+    tree.heading("#7", text="Сумма чека")
+    tree.column("#7", width=110)
 
     for item in get_all_from_database():
         tree.insert("", END, values=item)
     tree.place(relx=0.013, rely=0.13)
 
-    for index, total in enumerate(get_all_from_database()):
-        total_sum = tkinter.Label(text=f"Итоговый чек: {total[6]}")
-        total_sum.place(x=500, y=300)
-        total_sum.config(fg="Green")
-        if index == 0:
-            break
+    # for index, total in enumerate(get_all_from_database()):
+    #     total_sum = tkinter.Label(text=f"Итоговый чек: {total[6]}")
+    #     total_sum.place(x=500, y=300)
+    #     total_sum.config(fg="Green")
+    #     if index == 0:
+    #         break
 
     scroll = tkinter.Scrollbar(window, orient="vertical", command=tree.yview)
-    scroll.place(x=777, y=53, height=225)
+    scroll.place(x=884, y=53, height=225)
     tree.configure(yscrollcommand=scroll.set)
 
 
