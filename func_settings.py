72a58cb81fc5069effa3378cced2f58991b751eb
TurtleOld <shurpo.alexander@gmail.com>
2021-05-18 22:26:30 +0300

    функция перевода названия месяца в их число

---
+++ func_settings.py
@@ -0,0 +1,25 @@
+def get_digits_month(month):
+    if month == "Январь":
+        return 1
+    if month == "Февраль":
+        return 2
+    if month == "Март":
+        return 3
+    if month == "Апрель":
+        return 4
+    if month == "Май":
+        return 5
+    if month == "Июнь":
+        return 6
+    if month == "Июль":
+        return 7
+    if month == "Август":
+        return 8
+    if month == "Сентябрь":
+        return 9
+    if month == "Октябрь":
+        return 10
+    if month == "Ноябрь":
+        return 11
+    if month == "Декабрь":
+        return 12
